a4aaf9299dca93e3a6b6e74b5a915475d1d3ffba
ChristinOtniel <sotodonouchristinotniel@gmail.com>
2024-05-23 08:53:05 -0400

    ajout de modification

--- components/LocaleProvider.jsx
+++ components/LocaleProvider.jsx
@@ -1,9 +1,7 @@
-'use client'
-
-import { createContext, useContext, useState } from 'react'
-import { IntlProvider } from 'react-intl'
-import englishData from '@/i18n/en.json'
-import frenchData from '@/i18n/fr.json'
+import { createContext, useContext, useEffect, useState } from 'react';
+import { IntlProvider } from 'react-intl';
+import englishData from '@/i18n/en.json';
+import frenchData from '@/i18n/fr.json';
 
 const translations = {
     en: englishData,
@@ -12,17 +10,33 @@ const translations = {
 
 const LocaleContext = createContext();
 
-export function LocaleProvider({children}) {
+export function LocaleProvider({ children }) {
     const [locale, setLocale] = useState('fr');
 
-    return <LocaleContext.Provider value={[locale, setLocale]}>
-        <IntlProvider locale={locale} messages={translations[locale]}>
-            {children}
-        </IntlProvider>
-    </LocaleContext.Provider>
+    // Effet pour récupérer la langue stockée dans localStorage au chargement de l'application
+    useEffect(() => {
+        const storedLocale = localStorage.getItem('locale');
+        // Utilisation de || pour garantir que storedLocale n'est pas null ou undefined
+        setLocale(storedLocale || 'fr');
+    }, []);
+
+
+    // Fonction pour changer la langue avec stockage dans localStorage
+    const setLocaleWithStorage = (newLocale) => {
+        localStorage.setItem('locale', newLocale);
+        setLocale(newLocale);
+    };
+
+    return (
+        <LocaleContext.Provider value={{ locale, setLocale: setLocaleWithStorage }}>
+            <IntlProvider locale={locale} messages={translations[locale]}>
+                {children}
+            </IntlProvider>
+        </LocaleContext.Provider>
+    );
 }
 
 export function useLocale() {
-    const [locale, setLocale] = useContext(LocaleContext);
+    const { locale, setLocale } = useContext(LocaleContext);
     return [locale, setLocale];
 }
